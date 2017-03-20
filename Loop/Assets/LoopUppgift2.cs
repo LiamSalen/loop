1785491276534f5fc9af61c2ae9d3289e927371f
Liam Morgan <Liamsipad@outlook.com>
2017-03-20 13:58:59 +0100

    20/03/17 13:30

--- Loop/Assets/LoopUppgift2.cs
+++ Loop/Assets/LoopUppgift2.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Collections;
 
 public class LoopUppgift2 : MonoBehaviour {
-    public int valA = 7;    
+    public float valA = 8;    
 
 	// Use this for initialization
 	void Start () {
@@ -11,24 +11,24 @@ public class LoopUppgift2 : MonoBehaviour {
         List<int> list = new List<int>();
         // Adds value to the list
         list.Add(38);
-        list.Add(70);
+        list.Add(8);
         list.Add(91);
-        list.Add(68);
+        list.Add(8);
         list.Add(71);
         list.Add(97);
         list.Add(74);
         list.Add(20);
         list.Add(62);
-        list.Add(10);
+        list.Add(8);
         // A loop that checks the list
         
         for (int i = 0; i < list.Count; i++)
         {
             // Checks if i and valA have the same value
-            if (i == valA)
+            if (list[i] == valA)
             {
-                // Prints the list
-                print(list[i]);
+                // Prints the position of the set value
+                print(i);
             }
         }
     }
